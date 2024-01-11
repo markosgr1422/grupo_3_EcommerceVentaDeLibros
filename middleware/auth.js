e7db04e5408210283286e48d32db83930de1fd0d
Bruno Gorosito <gorositobrunoalejandro@gmail.com>
2024-01-11 11:26:47 -0300

    enrutamiento y redirección dependiendo de usuario

--- middleware/auth.js
+++ middleware/auth.js
@@ -19,13 +19,13 @@ const auth = (req, res, next) => {
   // if ()
   if (!userAdmin) {
     const baseUrl = req.protocol + "://" + req.get("host");
-    return res.redirect(baseUrl + "/");
+    return res.redirect(baseUrl + "/login");
+
   }
   next();
 };
 
 const isAuthorized = (req, res) => {
-  console.log(req.session.user);
   if (req.session.user) {
     if (req.session.user) {
       return true;
