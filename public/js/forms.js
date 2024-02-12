d4f8a374eb7e1a29059c4fd48723104a4704e340
Roman Ruiz <romanruix@gmai.com>
2024-02-12 18:43:01 -0300

    mejorando validaciones en front

--- public/js/forms.js
+++ public/js/forms.js
@@ -1,22 +1,30 @@
 window.onload = function(){
-const formulario = document.querySelector(".formulario");
+    const formulario = document.querySelector(".formulario");
 
-formulario.addEventListener('submit', function(event) {
-    event.preventDefault();    
-    let campoTitulo = document.querySelector('input[name="titulo"]');
-    let campoDescripcion = document.querySelector('input[name="descripcion"]');
-    if (campoTitulo.value === "") {
-        alert("Título no debe estar vacío");
-        return;
-    }
-    if (campoTitulo.value.length <= 5) {
-        alert("Título debe tener al menos 5 caracteres");
-        return;
-    }
-    if (campoDescripcion.value.length <= 20) {
-        alert("El campo descripción debe tener al menos 20 caracteres");
-        return;
-    }
-    formulario.submit();
-});
+    formulario.addEventListener('submit', function(event) {
+        let errores = [];
+        let campoTitulo = document.querySelector('input[name="titulo"]');
+        let campoDescripcion = document.querySelector('input[name="descripcion"]');
+        
+        if (campoTitulo.value === "") {
+            errores.push("Título no debe estar vacío");
+        } 
+        if (campoTitulo.value.length <= 5) {
+            errores.push("Título debe tener al menos 5 caracteres");
+        }
+        if (campoDescripcion.value.length <= 20) {
+            errores.push("Descripción debe tener al menos 20 caracteres");
+        }
+        
+        if (errores.length > 0) {
+            event.preventDefault();
+            let ulErrores = document.querySelector("div.errores ul");
+            ulErrores.innerHTML = "";
+            for (let i = 0; i < errores.length; i++) {
+                ulErrores.innerHTML += "<li>" + errores[i] + "</li>";
+            }
+        } else {
+            formulario.submit();
+        }
+    });
 }
