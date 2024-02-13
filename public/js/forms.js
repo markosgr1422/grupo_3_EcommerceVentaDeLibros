1f2697384ba9d01afa1daaff9215bee16b623c1d
Roman Ruiz <romanruix@gmai.com>
2024-02-12 21:00:29 -0300

    validaciones front

--- public/js/forms.js
+++ public/js/forms.js
@@ -1,6 +1,28 @@
 window.onload = function(){
     const formulario = document.querySelector(".formulario");
 
+    document.getElementById('titulo').addEventListener('input', function() {
+        var titulo = this.value;
+        var mensajeTitulo = document.getElementById('mensajeTitulo');
+        
+        if (titulo.length < 5) {
+            mensajeTitulo.style.display = 'flex';
+        } else {
+            mensajeTitulo.style.display = 'none';
+        }
+    });
+    
+    document.getElementById('descripcion').addEventListener('input', function() {
+        var descripcion = this.value;
+        var mensajeDescripcion = document.getElementById('mensajeDescripcion');
+        
+        if (descripcion.length < 20) {
+            mensajeDescripcion.style.display = 'flex';
+        } else {
+            mensajeDescripcion.style.display = 'none';
+        }
+    });
+
     formulario.addEventListener('submit', function(event) {
         let errores = [];
         let campoTitulo = document.querySelector('input[name="titulo"]');
@@ -15,16 +37,16 @@ window.onload = function(){
         if (campoDescripcion.value.length <= 20) {
             errores.push("Descripción debe tener al menos 20 caracteres");
         }
-        
         if (errores.length > 0) {
             event.preventDefault();
             let ulErrores = document.querySelector("div.errores ul");
             ulErrores.innerHTML = "";
             for (let i = 0; i < errores.length; i++) {
-                ulErrores.innerHTML += "<li>" + errores[i] + "</li>";
+                ulErrores.innerHTML += "<li> Error: " + errores[i] + "</li>";
             }
         } else {
             formulario.submit();
         }
+        
     });
 }
